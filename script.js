67cb5bef5950f5cbb482cc5416ebe4a685e8c2bb
NikitaSder <nikitacigankov6@gmail.com>
2026-01-17 23:45:26 +0200

    Додаток повідомлення про те що товар додан до кошика (кошика ще немає)

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function openNotif() {
 function closeNotif() {
   document.querySelector(".notif").style.display = "none";
 }
+
+
+function showToast(product) {
+  let toast = document.querySelector(".toast");
+  toast.textContent = `Товар '${product}' був додан до кошика.`;
+  toast.classList.add("show");
+  
+  let basketCountElem = document.querySelector(".basket_item");
+  let currentCount = parseInt(basketCountElem.textContent);
+  basketCountElem.textContent = currentCount + 1;
+  
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 5000);
+}
+
